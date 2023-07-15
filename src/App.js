e6aea2a5dcb06f4c020d698dfa816a69139df8ba
myeongjin <seomyeongjin99@gmail.com>
2023-07-15 17:10:57 +0900

    "Feat : 회원가입 페이지 구현"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LogInPage from "./pages/LoginPage";
+import LogInPage from "./pages/LogInPage";
 import AccountPage from "./pages/AccountPage";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
@@ -11,11 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LogInPage />} />
         <Route path="/AccountPage" element={<AccountPage />} />
         <Route path="MainPage" element={<MainPage />} />
         <Route path="/DetailPage" element={<DetailPage />} />
-        <Route path="/WritePage" element={<WritingPage />} />
+        <Route path="/WritingPage" element={<WritingPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/WeatherMenu.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import sun from "../images/sun.png";
+import cloudy from "../images/cloudy.png";
+import rain from "../images/rain.png";
+import snow from "../images/snow.png";
+
+const WeatherMenu = () => {
+  return (
+    <WeatherItemsContainer>
+      <WeatherItem>
+        <img src={sun} width="40px" height="40px"></img>맑음!
+      </WeatherItem>
+      <WeatherItem>
+        <img src={cloudy} width="40px" height="40px"></img>흐림!
+      </WeatherItem>
+      <WeatherItem>
+        <img src={rain} width="40px" height="40px"></img>비옴!
+      </WeatherItem>
+      <WeatherItem>
+        <img src={snow} width="40px" height="40px"></img>눈옴!
+      </WeatherItem>
+    </WeatherItemsContainer>
+  );
+};
+
+const WeatherItemsContainer = styled.div`
+  border: 2px solid black;
+  border-radius: 8px;
+  width: 100px;
+  height: 170px;
+  margin-left: 10px;
+`;
+
+const WeatherItem = styled.div`
+  width: 100px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+`;
+export default WeatherMenu;
